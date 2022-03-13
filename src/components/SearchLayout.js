ac523be3f6fd7ba3c9f147b88165f80555e98c25
sanghyun8039 <chltkdgus10@naver.com>
2022-03-13 22:31:58 +0900

    ADD DATE

--- src/components/SearchLayout.js
+++ src/components/SearchLayout.js
@@ -5,22 +5,70 @@ const SearchLayout = () =>
 {
     const [inputValue,setInputValue] = useState("");
     const [loading,setLoading] = useState(true);
-    const [price,setPrice] = useState([]);
+    const [tPrice,setTPrice] = useState([]);
+    const [oneMonthPrice,setOneMonthPrice] = useState([]);
+    const [threeMonthPrice,setThreeMonthPrice] = useState([]);
+    const [sixMonthPrice,setSixMonthPrice] = useState([]);
+    const [oneYearPrice,setOneYearPrice] = useState([]);
+    
+    const today = new Date(2022,2,12);
+
+    const diffOneMonth = new Date(today);
+    const diffThreeMonth = new Date(today);
+    const diffSixMonth = new Date(today);
+    const diffOneYear = new Date(today);
+ 
+    diffOneMonth.setMonth(today.getMonth() - 1);
+    diffThreeMonth.setMonth(today.getMonth() - 3);
+    diffSixMonth.setMonth(today.getMonth() - 6);
+    diffOneYear.setFullYear(today.getFullYear() - 1);
+
     const onChange = (e) =>
     {
         setInputValue(e.target.value);
         
     }
+
+    const calcJsonValue = (json,date) =>
+    {
+        const value = json;
+        while(!value[date.toISOString().split('T')[0]])
+        {
+            date.setDate(date.getDate() - 1);
+        }
+
+        return value[date.toISOString().split('T')[0]];
+    }
     const getTickerValue = async() => 
     {
         const json = await(
-            await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${inputValue}&apikey=H65SRD1M6KH9R56U`)
+            await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${inputValue}&outputsize=full&apikey=H65SRD1M6KH9R56U`)
         ).json();
-        const date = new Date(2022,2,8);
-        setPrice(json["Time Series (Daily)"][date.toISOString().split('T')[0]])
-        console.log(price);
-    }
+        
+        //setTPrice(json["Time Series (Daily)"][today.toISOString().split('T')[0]])
+        setTPrice(calcJsonValue(json["Time Series (Daily)"],today));
 
+        //setOneMonthPrice(json["Time Series (Daily)"][diffOneMonth.toISOString().split('T')[0]])
+        setOneMonthPrice(calcJsonValue(json["Time Series (Daily)"],diffOneMonth));
+        
+        
+        //setThreeMonthPrice(json["Time Series (Daily)"][diffThreeMonth.toISOString().split('T')[0]])
+        setThreeMonthPrice(calcJsonValue(json["Time Series (Daily)"],diffThreeMonth));
+        
+        
+        //setSixMonthPrice(json["Time Series (Daily)"][diffSixMonth.toISOString().split('T')[0]])
+        setSixMonthPrice(calcJsonValue(json["Time Series (Daily)"],diffSixMonth));
+        
+        
+        //setOneYearPrice(json["Time Series (Daily)"][diffOneYear.toISOString().split('T')[0]])
+        setOneYearPrice(calcJsonValue(json["Time Series (Daily)"],diffOneYear));
+        console.log(tPrice);
+        console.log(oneMonthPrice);
+        console.log(threeMonthPrice);
+        console.log(sixMonthPrice);
+        console.log(oneYearPrice);
+    }
+  
     const onSubmit = (e) =>
     {
         e.preventDefault();
@@ -47,9 +95,30 @@ const SearchLayout = () =>
                 {loading?"":
                 <div>
                     <h1>{inputValue}</h1>
-                <ul>
-                    {Object.keys(price).map(g=><li key={g}>{g} : {price[g]}</li>)}
-                </ul>
+                    <h3>{today.toISOString().split('T')[0]}</h3>
+                    <ul>
+                        {Object.keys(tPrice).map(g=><li key={g}>{g} : {tPrice[g]}</li>)}
+                    </ul>
+
+                    <h3>{diffOneMonth.toISOString().split('T')[0]}</h3>
+                    <ul>
+                        {Object.keys(oneMonthPrice).map(g=><li key={g}>{g} : {oneMonthPrice[g]}</li>)}
+                    </ul>
+
+                    <h3>{diffThreeMonth.toISOString().split('T')[0]}</h3>
+                    <ul>
+                        {Object.keys(threeMonthPrice).map(g=><li key={g}>{g} : {threeMonthPrice[g]}</li>)}
+                    </ul>
+
+                    <h3>{diffSixMonth.toISOString().split('T')[0]}</h3>
+                    <ul>
+                        {Object.keys(sixMonthPrice).map(g=><li key={g}>{g} : {sixMonthPrice[g]}</li>)}
+                    </ul>
+
+                    <h3>{diffOneYear.toISOString().split('T')[0]}</h3>
+                    <ul>
+                        {Object.keys(oneYearPrice).map(g=><li key={g}>{g} : {oneYearPrice[g]}</li>)}
+                    </ul>
                 </div>
                 }
             </div>

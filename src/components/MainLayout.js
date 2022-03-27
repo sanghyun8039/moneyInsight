1dc7e6b6940a2f19ca5737231c4bf509f9f4e0e8
sanghyun8039 <chltkdgus10@naver.com>
2022-03-27 20:33:05 +0900

    22/03/27

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -6,14 +6,25 @@ import { useTable } from 'react-table';
 const MainLayout = ({ticker}) =>
 {
   const [tickerValue,setTickerValue]= useState([]);
+  const [tClosePrice,setTClosePrice] = useState(0);
   const [tPrice,setTPrice] = useState([]);
+  const [loading,setLoading] = useState(false);
   const [oneMonthPrice,setOneMonthPrice] = useState([]);
+  const [threeMonthPrice,setThreeMonthPrice] = useState([]);
+  const [sixMonthPrice,setSixMonthPrice] = useState([]);
+  const [oneYearPrice,setOneYearPrice] = useState([]);
   const [data, setPrices] = useState([]);
   
   const today = new Date();
-  today.setDate(today.getDate() -1);
+  today.setDate(today.getDate() -2);
   const diffOneMonth = new Date(today);
   diffOneMonth.setMonth(today.getMonth() - 1);
+  const diffThreeMonth = new Date(today);
+  diffThreeMonth.setMonth(today.getMonth() - 3);
+  const diffSixMonth = new Date(today);
+  diffSixMonth.setMonth(today.getMonth() - 6);
+  const diffOneYear = new Date(today);
+  diffOneYear.setFullYear(today.getFullYear() - 1);
   
   const calcJsonValue = (json,date) =>
   {
@@ -63,24 +74,34 @@ const MainLayout = ({ticker}) =>
   //   }
   useEffect(()=> 
     {
-        
         const fetchData = async() => 
         {
           try{
           const json = await(
             await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=SPY&outputsize=full&apikey=H65SRD1M6KH9R56U`)
               ).json();
-          //setTickerValue(json["Time Series (Daily)"]); 
-
-          const tPriceValue = calcJsonValue(json["Time Series (Daily)"],today);
-          const newObject = {
-            '6. date' : diffOneMonth.toISOString().split('T')[0],
-          }
-          setTPrice(tPriceValue);
+          setTickerValue(json["Time Series (Daily)"]); 
+          
+          
+          
+          setTPrice(calcJsonValue(json["Time Series (Daily)"],today));
           setTPrice(prevState => ({ ...prevState, '6. date': today.toISOString().split('T')[0]}));
           setOneMonthPrice(calcJsonValue(json["Time Series (Daily)"],diffOneMonth));
-          setOneMonthPrice(prevState => ({ ...prevState, ...newObject}));
-          setDataPrice(tPrice,oneMonthPrice);
+          setOneMonthPrice(prevState => ({ ...prevState, '6. date' : diffOneMonth.toISOString().split('T')[0]}));
+          setThreeMonthPrice(calcJsonValue(json["Time Series (Daily)"],diffThreeMonth));
+          setThreeMonthPrice(prevState => ({ ...prevState, '6. date': diffThreeMonth.toISOString().split('T')[0]}));
+          setSixMonthPrice(calcJsonValue(json["Time Series (Daily)"],diffSixMonth));
+          setSixMonthPrice(prevState => ({ ...prevState, '6. date': diffSixMonth.toISOString().split('T')[0]}));
+          setOneYearPrice(calcJsonValue(json["Time Series (Daily)"],diffOneYear));
+          setOneYearPrice(prevState => ({ ...prevState, '6. date': diffOneYear.toISOString().split('T')[0]}));
+          
+          
+          
+          
+          
+          
+          setLoading(true);
+          
           }
           catch(error)
           {
@@ -91,22 +112,41 @@ const MainLayout = ({ticker}) =>
         fetchData();   
         
     },[]);
-    const setDataPrice = (tPrice,oneMonthPrice) =>
+    const setDataPrice = (tPrice,oneMonthPrice,threeMonthPrice,sixMonthPrice,oneYearPrice) =>
     {
       setPrices((currentValue) => [tPrice,...currentValue]);
       setPrices((currentValue) => [oneMonthPrice,...currentValue]);
+      setPrices((currentValue) => [threeMonthPrice,...currentValue]);
+      setPrices((currentValue) => [sixMonthPrice,...currentValue]);
+      setPrices((currentValue) => [oneYearPrice,...currentValue]);
     }
   //   useEffect(()=> {
   //     setPrices((currentValue) => [tPrice,...currentValue]);
   //     setPrices((currentValue) => [oneMonthPrice,...currentValue]);
   // },[oneMonthPrice]);
     
+    const setClosePrice = (today) =>
+    {
+      let todayPrice = parseFloat(tickerValue[today.toISOString().split('T')[0]]['4. close']); 
+      setTClosePrice((currentValue) => currentValue = todayPrice);
+    }
+
     useEffect(()=>
     {
+      if(loading)
+      {      
+        setClosePrice(today); 
+        //console.log(tClosePrice);
+        console.log(tickerValue);
+        setDataPrice(tPrice,oneMonthPrice,threeMonthPrice,sixMonthPrice,oneYearPrice);
+      }
+      
+    },[loading]);
 
-      
-      
-    },[oneMonthPrice]);
+    useEffect(()=>
+    {
+      console.log(tClosePrice);
+    },[tClosePrice])
 
   // useEffect(()=>
   // {
